3f179c7e1cd70ef0b952ebd149f6fb429f60f779
clement-scherrer <clement.scherrer@new-immo-group.com>
2026-09-13 09:33:52 +0200

    fix: rename + admin account show on website

--- tests/Controller/SecurityTest.php
+++ tests/Controller/SecurityTest.php
@@ -23,6 +23,27 @@ class SecurityTest extends AdminWebTestCase
         self::assertResponseRedirects('/admin');
     }
 
+    /**
+     * Quitter l'administration et se deconnecter aboutissent tous deux a l'accueil.
+     * La pastille est le seul indice permettant de distinguer les deux etats, et
+     * evite qu'un benevole reparte en croyant avoir ferme sa session.
+     */
+    public function testPublicHeaderShowsTheSessionBadgeOnlyWhenLoggedIn(): void
+    {
+        $crawler = $this->client->request('GET', '/');
+
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('header', 'Espace bénévoles');
+        self::assertSelectorNotExists('header a[href="/admin"]');
+
+        $this->client->loginUser($this->createAdmin());
+        $crawler = $this->client->request('GET', '/');
+
+        self::assertSelectorTextContains('header', 'Connecté');
+        self::assertSelectorExists('header a[href="/admin"]');
+        self::assertSelectorTextNotContains('header', 'Espace bénévoles');
+    }
+
     public function testWrongPasswordIsRejected(): void
     {
         $this->createAdmin();
